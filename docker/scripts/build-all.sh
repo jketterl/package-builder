8de3a597f36b16dfdf7afc38a9eed029aebf3aeb
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-12-29 00:05:52 +0100

    install packages (in case of dependencies)

--- docker/scripts/build-all.sh
+++ docker/scripts/build-all.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+mkdir -p /tmp/output
 mkdir -p /tmp/build
 cd /tmp/build
 
 for PACKAGE in `ls /packages`; do
     echo "Building package $PACKAGE"
     /packages/$PACKAGE/build.sh
+
+    dpkg -i *.deb
+    for DEB in `ls *.deb`; do
+        mv $DEB /tmp/output
+    done
 done
 
 export GPG_TTY=$(tty)
 gpg --batch --import <(echo "$SIGN_KEY")
+
+cd /tmp/output
 
 for PACKAGE in `ls *.deb`; do
     debsigs --sign=maint -k $SIGN_KEY_ID $PACKAGE
