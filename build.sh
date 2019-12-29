b1927bc23c7f9e3d8334f52622964be8ccf1b85b
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-12-29 21:08:42 +0100

    separate artifacts by arch

--- build.sh
+++ build.sh
@@ -11,13 +11,12 @@ fi
 
 for DIST in `cat dists/$ARCH`; do
     TAG=${DIST//[:]/_}
-    echo $TAG
 
     docker build --pull --build-arg BASE=$DIST -f docker/Dockerfile-debian -t package-builder:${TAG}_latest .
     docker run --name package-builder -e SIGN_KEY_ID="$SIGN_KEY_ID" -e SIGN_KEY="$SIGN_KEY" package-builder:${TAG}_latest $@
     docker cp $CONTAINER_NAME:/packages.tar.gz .
     docker rm $CONTAINER_NAME
-    mkdir -p output/${TAG}
-    tar xvfz packages.tar.gz -C output/${TAG}
+    mkdir -p output/${ARCH}/${TAG}
+    tar xvfz packages.tar.gz -C output/${ARCH}/${TAG}
     rm packages.tar.gz
 done
