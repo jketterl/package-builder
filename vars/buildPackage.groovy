867de6df0a9578143302f5c7d1614269fa87b20f
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-01-05 14:43:24 +0100

    let's try without the post steps

--- vars/buildPackage.groovy
+++ vars/buildPackage.groovy
@@ -1,14 +1,9 @@
 def call(Map params) {
     steps {
+        git 'https://github.com/jketterl/package-builder.git'
+        sh 'rm -rf output/'
         withCredentials([file(credentialsId: params.gpgsigningkey, variable: "SIGN_KEY_FILE")]) {
-            git 'https://github.com/jketterl/package-builder.git'
-            sh 'rm -rf output/'
             sh "./run.sh ${params.pack}"
-        }
-    }
-    post {
-        always {
-            s3Upload consoleLogLevel: 'INFO', dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: 'de.dd5jfk.openwebrx.debian-packages', excludedFile: '', flatten: false, gzipFiles: false, keepForever: false, managedArtifacts: false, noUploadOnFailure: true, selectedRegion: 'eu-central-1', showDirectlyInBrowser: false, sourceFile: 'output/**/*.deb', storageClass: 'STANDARD', uploadFromSlave: true, useServerSideEncryption: false]], pluginFailureResultConstraint: 'FAILURE', profileName: params.S3profile, userMetadata: []
         }
     }
 }
