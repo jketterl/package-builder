ec7179c9a68a15209776e1ad60874ed0b833c283
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-01-05 14:33:47 +0100

    let's try the params approach

--- vars/buildPackage.groovy
+++ vars/buildPackage.groovy
@@ -1,15 +1,15 @@
-def call(String arch, String pack, String gpgsigningkey, String S3profile) {
-    agent { label arch }
+def call(Map params) {
+    agent { label params.arch }
     steps {
-        withCredentials([file(credentialsId: gpgsigningkey, variable: "SIGN_KEY_FILE")]) {
+        withCredentials([file(credentialsId: params.gpgsigningkey, variable: "SIGN_KEY_FILE")]) {
             git 'https://github.com/jketterl/package-builder.git'
             sh 'rm -rf output/'
-            sh "./run.sh ${pack}"
+            sh "./run.sh ${params.pack}"
         }
     }
     post {
         always {
-            s3Upload consoleLogLevel: 'INFO', dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: 'de.dd5jfk.openwebrx.debian-packages', excludedFile: '', flatten: false, gzipFiles: false, keepForever: false, managedArtifacts: false, noUploadOnFailure: true, selectedRegion: 'eu-central-1', showDirectlyInBrowser: false, sourceFile: 'output/**/*.deb', storageClass: 'STANDARD', uploadFromSlave: true, useServerSideEncryption: false]], pluginFailureResultConstraint: 'FAILURE', profileName: S3profile, userMetadata: []
+            s3Upload consoleLogLevel: 'INFO', dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: 'de.dd5jfk.openwebrx.debian-packages', excludedFile: '', flatten: false, gzipFiles: false, keepForever: false, managedArtifacts: false, noUploadOnFailure: true, selectedRegion: 'eu-central-1', showDirectlyInBrowser: false, sourceFile: 'output/**/*.deb', storageClass: 'STANDARD', uploadFromSlave: true, useServerSideEncryption: false]], pluginFailureResultConstraint: 'FAILURE', profileName: params.S3profile, userMetadata: []
         }
     }
 }
