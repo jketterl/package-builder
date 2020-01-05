37095c73488b72510aeefa4c2f31450933e3c9b0
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-01-05 14:18:21 +0100

    fix variable names

--- vars/buildPackage.groovy
+++ vars/buildPackage.groovy
@@ -1,4 +1,4 @@
-def call(String arch, String pack, String gpgsigningkey, String S3profile) {
+def call(String pack, String gpgsigningkey, String S3profile) {
     steps {
         withCredentials([file(credentialsId: gpgsigningkey, variable: "SIGN_KEY_FILE")]) {
             git 'https://github.com/jketterl/package-builder.git'
@@ -8,7 +8,7 @@ def call(String arch, String pack, String gpgsigningkey, String S3profile) {
     }
     post {
         always {
-            s3Upload consoleLogLevel: 'INFO', dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: 'de.dd5jfk.openwebrx.debian-packages', excludedFile: '', flatten: false, gzipFiles: false, keepForever: false, managedArtifacts: false, noUploadOnFailure: true, selectedRegion: 'eu-central-1', showDirectlyInBrowser: false, sourceFile: 'output/**/*.deb', storageClass: 'STANDARD', uploadFromSlave: true, useServerSideEncryption: false]], pluginFailureResultConstraint: 'FAILURE', profileName: S3Profile, userMetadata: []
+            s3Upload consoleLogLevel: 'INFO', dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: 'de.dd5jfk.openwebrx.debian-packages', excludedFile: '', flatten: false, gzipFiles: false, keepForever: false, managedArtifacts: false, noUploadOnFailure: true, selectedRegion: 'eu-central-1', showDirectlyInBrowser: false, sourceFile: 'output/**/*.deb', storageClass: 'STANDARD', uploadFromSlave: true, useServerSideEncryption: false]], pluginFailureResultConstraint: 'FAILURE', profileName: S3profile, userMetadata: []
         }
     }
 }
