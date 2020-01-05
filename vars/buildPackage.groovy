ae67617d2529b67c1945b9ef00c19f3f9c24b6cd
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-01-05 16:25:29 +0100

    escape all the things

--- vars/buildPackage.groovy
+++ vars/buildPackage.groovy
@@ -10,31 +10,31 @@ set -euo pipefail
 CONTAINER_NAME=package-builder
 ARCH=\$(uname -m)
 
-if [[ ! -z "${SIGN_KEY_FILE:-}" ]]; then
-    SIGN_KEY=\$(cat "$SIGN_KEY_FILE")
+if [[ ! -z "\${SIGN_KEY_FILE:-}" ]]; then
+    SIGN_KEY=\$(cat "\$SIGN_KEY_FILE")
 fi
 
 BUILD_NUMBER_ARG=""
-if [[ ! -z "${BUILD_NUMBER:-}" ]]; then
-    BUILD_NUMBER_ARG="-e BUILD_NUMBER=${BUILD_NUMBER}"
+if [[ ! -z "\${BUILD_NUMBER:-}" ]]; then
+    BUILD_NUMBER_ARG="-e BUILD_NUMBER=\${BUILD_NUMBER}"
 fi
 
 for DIST in `cat dists/$ARCH`; do
-    OUTPUT_DIST=${DIST//[:]/_}
-    TAG = ${OUTPUT_DIST}_${ARCH}_latest
+    OUTPUT_DIST=\${DIST//[:]/_}
+    TAG = \${OUTPUT_DIST}_\${ARCH}_latest
 
-    docker pull 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${TAG}
+    docker pull 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:\${TAG}
     RC = 0
-    docker run --name package-builder -e SIGN_KEY_ID="${SIGN_KEY_ID}" -e SIGN_KEY="$SIGN_KEY" ${BUILD_NUMBER_ARG} 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${TAG} ${params.pack} || RC=$?
-    if [[ ${RC} ]]; then
-        docker cp ${CONTAINER_NAME}:/packages.tar.gz . || RC=$?
+    docker run --name package-builder -e SIGN_KEY_ID="\${SIGN_KEY_ID}" -e SIGN_KEY="\$SIGN_KEY" \${BUILD_NUMBER_ARG} 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:\${TAG} ${params.pack} || RC=\$?
+    if [[ \${RC} ]]; then
+        docker cp \${CONTAINER_NAME}:/packages.tar.gz . || RC=\$?
     fi
-    docker rm ${CONTAINER_NAME}
-    if [[ ! ${RC} ]]; then
-        exit ${RC}
+    docker rm \${CONTAINER_NAME}
+    if [[ ! \${RC} ]]; then
+        exit \${RC}
     fi
-    mkdir -p output/${ARCH}/${OUTPUT_DIST}
-    tar xvfz packages.tar.gz -C output/${ARCH}/${OUTPUT_DIST}
+    mkdir -p output/\${ARCH}/\${OUTPUT_DIST}
+    tar xvfz packages.tar.gz -C output/\${ARCH}/\${OUTPUT_DIST}
     rm packages.tar.gz
 done
     """
