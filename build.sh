7cdb94d67eeb6c38e2347d979595a5a1790f915b
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-01-05 02:57:40 +0100

    prepare for ECR usage

--- build.sh
+++ build.sh
@@ -1,33 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SRC=$(realpath packages)
-CONTAINER_NAME=package-builder
 ARCH=$(uname -m)
 
-if [[ ! -z "${SIGN_KEY_FILE:-}" ]]; then
-    SIGN_KEY=$(cat "$SIGN_KEY_FILE")
-fi
-
-BUILD_NUMBER_ARG=""
-if [[ ! -z "${BUILD_NUMBER:-}" ]]; then
-    BUILD_NUMBER_ARG="-e BUILD_NUMBER=${BUILD_NUMBER}"
-fi
-
 for DIST in `cat dists/$ARCH`; do
-    TAG=${DIST//[:]/_}
+    TAG=${DIST//[:]/_}_${ARCH}_latest
 
-    docker build --pull --build-arg BASE=$DIST -f docker/Dockerfile-debian -t package-builder:${TAG}_latest .
-    RC=0
-    docker run --name package-builder -e SIGN_KEY_ID="${SIGN_KEY_ID}" -e SIGN_KEY="$SIGN_KEY" ${BUILD_NUMBER_ARG} package-builder:${TAG}_latest $@ || RC=$?
-    if [[ ${RC} ]]; then
-        docker cp ${CONTAINER_NAME}:/packages.tar.gz . || RC=$?
-    fi
-    docker rm ${CONTAINER_NAME}
-    if [[ ! ${RC} ]]; then
-        exit ${RC}
-    fi
-    mkdir -p output/${ARCH}/${TAG}
-    tar xvfz packages.tar.gz -C output/${ARCH}/${TAG}
-    rm packages.tar.gz
+    docker build --pull --build-arg BASE=$DIST -f docker/Dockerfile-debian -t 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${TAG} .
+    docker push 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${TAG}
 done
---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CONTAINER_NAME=package-builder
+ARCH=$(uname -m)
+
+if [[ ! -z "${SIGN_KEY_FILE:-}" ]]; then
+    SIGN_KEY=$(cat "$SIGN_KEY_FILE")
+fi
+
+BUILD_NUMBER_ARG=""
+if [[ ! -z "${BUILD_NUMBER:-}" ]]; then
+    BUILD_NUMBER_ARG="-e BUILD_NUMBER=${BUILD_NUMBER}"
+fi
+
+for DIST in `cat dists/$ARCH`; do
+    OUTPUT_DIST=${DIST//[:]/_}
+    TAG=${OUTPUT_DIST}_${ARCH}_latest
+
+    docker pull 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${TAG}
+    RC=0
+    docker run --name package-builder -e SIGN_KEY_ID="${SIGN_KEY_ID}" -e SIGN_KEY="$SIGN_KEY" ${BUILD_NUMBER_ARG} 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${TAG} $@ || RC=$?
+    if [[ ${RC} ]]; then
+        docker cp ${CONTAINER_NAME}:/packages.tar.gz . || RC=$?
+    fi
+    docker rm ${CONTAINER_NAME}
+    if [[ ! ${RC} ]]; then
+        exit ${RC}
+    fi
+    mkdir -p output/${ARCH}/${OUTPUT_DIST}
+    tar xvfz packages.tar.gz -C output/${ARCH}/${OUTPUT_DIST}
+    rm packages.tar.gz
+done
