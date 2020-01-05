e17a685b5432aa1c269536134999d2c3cac4d48a
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-01-05 14:45:47 +0100

    maybe without the steps

--- vars/buildPackage.groovy
+++ vars/buildPackage.groovy
@@ -1,9 +1,7 @@
 def call(Map params) {
-    steps {
-        git 'https://github.com/jketterl/package-builder.git'
-        sh 'rm -rf output/'
-        withCredentials([file(credentialsId: params.gpgsigningkey, variable: "SIGN_KEY_FILE")]) {
-            sh "./run.sh ${params.pack}"
-        }
+    git 'https://github.com/jketterl/package-builder.git'
+    sh 'rm -rf output/'
+    withCredentials([file(credentialsId: params.gpgsigningkey, variable: "SIGN_KEY_FILE")]) {
+        sh "./run.sh ${params.pack}"
     }
 }
