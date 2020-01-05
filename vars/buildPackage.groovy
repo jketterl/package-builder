f5cb5e6640c40830f8f4536fe0bc95951d2cdc00
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-01-05 14:06:45 +0100

    add custom build pipeline step

---
+++ vars/buildPackage.groovy
@@ -0,0 +1,14 @@
+def call(String arch, String pack, String gpgsigningkey, String S3profile) {
+    steps {
+        withCredentials([file(credentialsId: gpgsigningkey, variable: "SIGN_KEY_FILE")]) {
+            git 'https://github.com/jketterl/package-builder.git'
+            sh 'rm -rf output/'
+            sh "./run.sh ${pack}"
+        }
+    }
+    post {
+        always {
+            s3Upload consoleLogLevel: 'INFO', dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: 'de.dd5jfk.openwebrx.debian-packages', excludedFile: '', flatten: false, gzipFiles: false, keepForever: false, managedArtifacts: false, noUploadOnFailure: true, selectedRegion: 'eu-central-1', showDirectlyInBrowser: false, sourceFile: 'output/**/*.deb', storageClass: 'STANDARD', uploadFromSlave: true, useServerSideEncryption: false]], pluginFailureResultConstraint: 'FAILURE', profileName: S3Profile, userMetadata: []
+        }
+    }
+}
