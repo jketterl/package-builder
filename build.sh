fb8fb51073c3c63a7e9aab9ddb5f87ad2278f35c
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-01-12 17:19:38 +0100

    all is built, cache back on

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ ARCH=$(uname -m)
 for DIST in `cat dists/$ARCH`; do
     TAG=${DIST//[:]/_}_${ARCH}_latest
 
-    docker build --no-cache --pull --build-arg BASE=$DIST -f docker/Dockerfile-debian -t 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${TAG} .
+    docker build --pull --build-arg BASE=$DIST -f docker/Dockerfile-debian -t 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${TAG} .
     docker push 768356633999.dkr.ecr.eu-central-1.amazonaws.com/package-builder:${TAG}
 done
