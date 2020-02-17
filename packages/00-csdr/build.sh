e4278f85c2241bd67a2b6101ebdc8c638cecbc2c
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-02-17 23:08:02 +0100

    fix branch name

--- packages/00-csdr/build.sh
+++ packages/00-csdr/build.sh
@@ -8,7 +8,7 @@ fi
 git clone --depth 1 ${BRANCH_ARG} https://github.com/jketterl/csdr.git
 pushd csdr
 if [[ ! -z ${BUILD_NUMBER:-} ]]; then
-  GBP_ARGS="--debian-branch=debian --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
+  GBP_ARGS="--debian-branch=master --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
   gbp dch ${GBP_ARGS}
 fi
 debuild -us -uc
