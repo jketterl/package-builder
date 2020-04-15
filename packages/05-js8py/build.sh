659dd1e3383d18f7cf7b89f5106c4f3d64dde033
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-04-15 21:19:21 +0200

    add js8py package

---
+++ packages/05-js8py/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ $(uname -m) == "x86_64" ]]; then
+    BRANCH_ARG=""
+    if [[ ! -z ${RELEASE_BRANCH:-} ]]; then
+        BRANCH_ARG="-b ${RELEASE_BRANCH}"
+    fi
+
+    git clone --depth 1 ${BRANCH_ARG} https://github.com/jketterl/js8py.git
+    pushd js8py
+    if [[ ! -z ${BUILD_NUMBER:-} ]]; then
+        GBP_ARGS="--debian-branch=develop --snapshot --auto --snapshot-number=${BUILD_NUMBER}"
+        gbp dch ${GBP_ARGS}
+    fi
+    debuild -us -uc
+    popd
+fi
